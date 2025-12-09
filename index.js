f8b41a1e09c90aa3e1a9b4b4928b45e4fcb4f151
ibrahimvau5518 <jibonm676@gmail.com>
2025-12-09 11:50:13 +0600

    add admin verification middleware

--- index.js
+++ index.js
@@ -73,3 +73,14 @@ const verifyToken = (req, res, next) => {
     next();
   });
 };
+
+const verifyAdmin = async (req, res, next) => {
+  const email = req.user?.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const isAdmin = user?.role === 'admin';
+  if (!isAdmin) {
+    return res.status(403).send({ massage: 'forbidden access' });
+  }
+  next();
+};
