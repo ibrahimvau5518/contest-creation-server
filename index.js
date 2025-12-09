9984a542d223503bce1335b4713d4e977203b875
ibrahimvau5518 <jibonm676@gmail.com>
2025-12-09 11:48:37 +0600

    configure MongoDB client and initialize database collections

--- index.js
+++ index.js
@@ -14,3 +14,38 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4rgvatj.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let userCollection,
+  creatorCollection,
+  registerContest,
+  paymentsCollection,
+  winCollection,
+  upcomingCollection;
+
+async function run() {
+  try {
+    userCollection = client.db('ContestHub').collection('AllUser');
+    creatorCollection = client.db('ContestHub').collection('creatorContest');
+    registerContest = client.db('ContestHub').collection('register');
+    paymentsCollection = client.db('ContestHub').collection('payments');
+    winCollection = client.db('ContestHub').collection('win');
+    upcomingCollection = client.db('ContestHub').collection('upcoming');
+
+    await client.db('admin').command({ ping: 1 });
+    console.log('Connected to MongoDB!');
+  } finally {
+  }
+}
+run().catch(console.dir);
